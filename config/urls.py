b53af39b44ed97ae224cdc95686939b125cd04ac
ZBlusBlusProgrammer <g.abandah@gmail.com>
2026-08-12 16:26:50 +0300

    Added core to apps, and enhanced urls.py

--- config/urls.py
+++ config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path , include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
-
+    path('', include('core.urls')),  # Routes root and dashboard paths to the core app
 ]
